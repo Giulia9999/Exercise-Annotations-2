aed210e515fc961c2169a95bbffb5cd5834d3c55
Giulia <giuliac35@gmail.com>
2023-02-15 19:42:26 +0100

    Create 2 methods in Greetings class, add DevAnnotation to them

--- src/Greetings.java
+++ src/Greetings.java
@@ -1,2 +1,8 @@
+
 public class Greetings {
+    @DevAnnotation(DevName = "John", DevSurname = "Walker")
+    public void sayWelcome(){}
+    
+    @DevAnnotation(DevName = "Mark", DevSurname = "Brown")
+    public void sayGoodbye(){}
 }
